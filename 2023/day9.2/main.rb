e1888d5be0a088bc447bb67c435837b82ee15cb2
Edward Holets <ned@labzero.com>
2023-12-09 11:19:56 -0800

    Day 9, part 2.

---
+++ 2023/day9.2/main.rb
@@ -0,0 +1,29 @@
+Sequence = Struct.new(:numbers) do
+    def zeros?
+        numbers.find { |n| !n.zero? }.nil?
+    end
+
+    def steps
+        if !@steps 
+            @steps = []
+            numbers.each_with_index do |n, i|
+                @steps.append(n - numbers[i - 1]) if i > 0 && i < numbers.length
+            end
+        end
+        @steps
+    end
+
+    def predict
+        return 0 if zeros?
+        step = Sequence.new(steps).predict
+        numbers.first - step
+    end
+end
+
+sequences = []
+File.readlines("input.txt", chomp: true).each do |line|
+    numbers = line.split.collect(&:to_i)
+    sequences.append(Sequence.new(numbers))
+end
+
+puts "Sum: #{sequences.sum(&:predict)}"
