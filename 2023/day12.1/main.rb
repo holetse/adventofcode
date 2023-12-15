f536a8458e180c6d0e51f559bec8bd809bc18e2f
Edward Holets <ned@labzero.com>
2023-12-14 20:21:23 -0800

    Day 12, part 1, faster method.

--- 2023/day12.1/main.rb
+++ 2023/day12.1/main.rb
@@ -1,121 +1,46 @@
-class Node
-    include Enumerable
-    attr_accessor :value, :operational, :damaged, :parent
-
-    def initialize(value, operational = nil, damaged = nil, parent = nil, *args)
-        self.value = value
-        self.operational = operational
-        self.damaged = damaged
-        self.parent = parent
-        super(*args)
-    end
-
-    def operational=(node)
-        @operational = node
-        node.parent = self if !node.nil?
-    end
-
-    def damaged=(node)
-        @damaged = node
-        node.parent = self if !node.nil?
-    end
-
-    def each
-        operational.each { |l| yield l } if !operational.nil?
-        yield self
-        damaged.each { |r| yield r } if !damaged.nil?
-    end
-
-    def leaves
-        if operational.nil? && damaged.nil?
-            yield self 
-        else
-            operational.leaves { |l| yield l } if !operational.nil?
-            damaged.leaves { |r| yield r } if !damaged.nil?
-        end
-    end
-    
-    def depth
-        count = 0
-        node = self
-        while node.parent
-            count += 1
-            node = node.parent
-        end
-
-        count
-    end
-
-    def branch_value
-        values = [value]
-        node = self
-        while node.parent
-            if node.parent.operational == node
-                values.append('.')
-            else
-                values.append('#')
-            end
-            node = node.parent
-            values.append(node.value)
-        end
-
-        values.reverse.join('')
-    end
-
-    def visualize_branch
-        values = [value]
-        node = self
-        while node.parent
-            node = node.parent
-            values.append('->', "#{node.value}?")
-        end
-
-        values.reverse.join('')
-    end
-
-    def to_s
-        "<'#{value}' operational=#{operational.inspect} damaged=#{damaged.inspect} parent=#{parent.inspect}>"
-    end
-end
-
 Record = Struct.new(:fragment, :spans) do
 
-    def spans_r
-        @spans_r ||= Regexp.new("^\\.*#{spans.collect {|c| "\#{#{c}}" }.join('\\.+')}\\.*$")
-    end
-    def possibilites
-        return @possibilites if !@possibilites.nil?
-
-        tree = nil
-        fixed = ''
-        fragment.split('').each do |c|
-            if (c == '?')
-                if tree.nil?
-                    tree = Node.new(fixed, Node.new(''), Node.new(''))
+    def consecutive_counts(char, str)
+        groups = []
+        group_length = nil
+    
+        str.each_char do |c|
+            if c == char
+                if group_length
+                    group_length += 1
                 else
-                    tree.leaves do |leaf|
-                        leaf.value += fixed
-                        leaf.operational = Node.new('')
-                        leaf.damaged = Node.new('')
-                    end
+                    group_length = 1
                 end
-                fixed = ''
             else
-                fixed += c
+                groups.append(group_length) if group_length
+                group_length = nil
             end
         end
-        if fixed.length > 0
-            tree.leaves do |leaf|
-                leaf.value += fixed
+    
+        groups.append(group_length) if group_length
+        groups
+    end
+    
+    def calculate_possibilites(fragment, spans)
+        if (unknown = fragment.index('?')).nil?
+            if consecutive_counts('#', fragment) == spans
+                return 1
+            else
+                return 0
             end
         end
-
-        possibilites = []
-        tree.leaves do |leaf|
-            possibility = leaf.branch_value
-            possibilites.append(possibility) if spans_r.match(possibility)
-        end
-        @possibilites = possibilites
+    
+        damaged_fragment = fragment.dup
+        damaged_fragment[unknown] = '.'
+    
+        operational_fragment = fragment.dup
+        operational_fragment[unknown] = '#'
+        
+        return calculate_possibilites(damaged_fragment, spans) + calculate_possibilites(operational_fragment, spans)
+    end
+    
+    def possibilites
+        @possibilites ||= calculate_possibilites(fragment, spans)
     end
 end
 
@@ -127,5 +52,5 @@ File.readlines("input.txt", chomp: true).each do |line|
 end
 
 
-puts "Possibilites: #{records.sum {|r| r.possibilites.length }}"
+puts "Possibilites: #{records.sum(&:possibilites)}"
 
