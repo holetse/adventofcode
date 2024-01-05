c9b734a44ac942d85815aee554d17ee6ea166660
Edward Holets <ned@labzero.com>
2024-01-05 15:33:00 -0800

    Remove unused grid code from 18.2.

--- 2023/day18.2/main.rb
+++ 2023/day18.2/main.rb
@@ -1,136 +1,3 @@
-Cell = Struct.new(:color, :row, :col, :matrix, :edge, :vertex, :covered, :included, :included_by, :multi_include) do
-    def neighbors
-        offsets = [
-            [-1, 0], [0, -1], [0, 1], [1, 0]
-        ]
-        @neighbors ||= offsets.collect { |o| matrix.get_cell(row + o.first, col + o.last) }.compact
-    end
-
-    def vertical_edge?
-        return false if !edge?
-        edge.first.x == edge.last.x
-    end
-
-    def horizontal_edge?
-        return false if !edge?
-        edge.first.y == edge.last.y
-    end
-
-    def edge_type
-        return '.' if !edge?
-        return 'v' if vertical_edge?
-        'h'
-    end
-
-    def colored?
-        !color.nil?
-    end
-
-    def edge?
-        !!edge
-    end
-
-    def vertex?
-        !!vertex
-    end
-
-    def inside?
-        return true if edge?
-        return @inside if @inside
-
-        crossed = 0
-        is_edge = false
-        started_vertex = nil
-        started_dir = nil
-        matrix.rows[row][0..col].each do |cell|
-            if !is_edge && cell.edge?
-                is_edge = true
-                started_vertex = cell.vertex
-                started_dir = matrix.get_cell(row - 1, cell.col)&.edge? ? 1 : -1 if started_vertex
-            end
-            if is_edge
-                if cell.vertex? && started_vertex != cell.vertex
-                    ended_dir = matrix.get_cell(row - 1, cell.col)&.edge? ? 1 : -1
-                    crossed += 1 if started_dir != ended_dir
-                    is_edge = false
-                    started_vertex = nil
-                    started_dir = nil
-                elsif !cell.edge?#!cell.connection_directions.include?([0, 1])
-                    crossed += 1 #if  #if started_edge != cell.edge #if ((cell.edge? || cell.vertex?) && started_dir == (cell.edge.first.y <=> cell.edge.last.y) || !cell.vertex?) #!cell.vertex?#if cell.connection_directions.include?([started_dir.first * -1, 0])
-                    is_edge = false
-                    started_vertex = nil
-                    started_dir = nil
-                end
-            end
-        end
-        raise "bad edge" if is_edge
-        @inside = crossed.odd?
-        #crossed.odd?
-    end
-
-    def inside_but_not_included
-        inside? && !included
-    end
-    
-    def to_s
-        inspect
-    end
-
-    def inspect
-        "<cell #{color}@(#{row}, #{col})>"
-    end
-end
-
-class Matrix
-    include Enumerable
-
-    def initialize(rows, *args)
-        @rows = rows.dup.freeze
-        super(*args)
-    end
-
-    def rows
-        @rows
-    end
-
-    def cols
-        @cols ||= 0.upto(rows[0].length - 1).collect { |i| rows.collect { |r| r[i] } }
-    end
-
-    def each
-        if block_given?
-            rows.each { |r| r.each { |c| yield c } }
-        else
-            Enumerator.new do |y|
-                rows.each { |r| r.each { |c| y << c } }
-            end
-        end
-    end
-
-    def each_row
-        rows.each { |r| yield r }
-    end
-
-    def each_col
-        cols.each { |c| yield c }
-    end
-
-    def get_cell(row, col)
-        return nil if row < 0 || row > rows.length - 1 || col < 0 || col > cols.length - 1
-        rows[row][col]
-    end
-
-    def visualize(method = :colored?)
-        rows.collect { |r| r.collect(&method).collect do |v|
-            if [true, false, nil].include?(v)
-                v ? '#' : '.'
-            else
-                v
-            end
-        end.join('') }.join("\n")
-    end
-end
-
 class Polygon
     attr_reader :vertices
     attr_reader :edges
@@ -150,10 +17,6 @@ class Polygon
             v1.x == v2.x
         end
 
-        # def direction
-        #     vertical_edge? ? (v1.y - v2.y <=> 0) : (v1.x - v2.x <=> 0)
-        # end
-
         def length
             (vertical_edge? ? (v2.y - v1.y) : (v2.x - v1.x)).abs
         end
@@ -169,7 +32,6 @@ class Polygon
         end
 
         def overlap?(edge_or_range)
-            #edge.range.begin <= range.end && range.begin <= edge.range.end
             other_range = edge_or_range.respond_to?(:range) ? edge_or_range.range : edge_or_range
             other_range.begin <= range.end && range.begin <= other_range.end 
         end
@@ -197,27 +59,6 @@ class Polygon
     
             return overlap_range, remainder
         end
-
-        # def map_range(rng)
-        #     mapped_range = nil
-        #     remainder = []
-        #     if range.cover?(rng)
-        #         mapped_range = map(rng.begin)..map(rng.end)
-        #     elsif rng.begin < range.begin && rng.end > range.end # begin & end overlap
-        #         mapped_range = dst_range
-        #         remainder = [rng.begin..(range.begin - 1), (range.end + 1)..rng.end]
-        #     elsif rng.begin >= range.begin && rng.end > range.end && rng.begin <= range.end  # end overlap
-        #         mapped_range = map(rng.begin)..(dst_range.end)
-        #         remainder = [(range.end + 1)..rng.end]
-        #     elsif rng.begin < range.begin && rng.end <= range.end && rng.end >= range.begin  # begin overlap
-        #         mapped_range = (dst_range.begin)..map(rng.end)
-        #         remainder = [rng.begin..(range.begin - 1)]
-        #     else
-        #         remainder = [rng]
-        #     end
-    
-        #     return mapped_range, remainder
-        # end
     end
 
     def initialize(*args)
@@ -255,18 +96,6 @@ class Polygon
 
     def extent
         [[bounds[0][0], bounds[1][0]], [bounds[0][1], bounds[1][1]]]
-    end
-
-    def point_to_grid(x, y)
-        min_x, max_x, min_y, max_y = extent.flatten
-        span_y = max_y - min_y
-        [span_y - (y - min_y), x - min_x] # y-axis flips
-    end
-
-    def grid_to_point(row, col)
-        min_x, max_x, min_y, max_y = extent.flatten
-        span_y = max_y - min_y
-        [ col + min_x, (span_y + min_y) - row]
     end
 
     def horizontal_edges
@@ -329,76 +158,7 @@ class Polygon
         crossings.odd?
     end
 
-    def grid
-        return @grid if @grid
-        min_x, max_x, min_y, max_y = extent.flatten
-        span_x = max_x - min_x
-        span_y = max_y - min_y
-        rows = 0.upto(span_y+1).collect do |row|
-            0.upto(span_x+1).collect do |col|
-                Cell.new(nil, row, col)
-            end
-        end
-        
-        @grid = Matrix.new(rows)
-        @grid.each do |cell|
-            cell.matrix = @grid
-        end
-
-        if @vertices.length > 1
-            @vertices.each_with_index do |v, vi|
-                next_v = @vertices[vi + 1]
-                next if next_v.nil?
-                row, col = point_to_grid(v.x, v.y)
-                next_row, next_col = point_to_grid(next_v.x, next_v.y)
-                vertical = v.x == next_v.x
-                @grid.get_cell(row, col).vertex = v
-                @grid.get_cell(next_row, next_col).vertex = next_v
-                if vertical
-                    start, finish = [row, next_row].minmax
-                    start.upto(finish) do |i|
-                        cell = @grid.get_cell(i, col)
-                    end
-                else
-                    start, finish = [col, next_col].minmax
-                    start.upto(finish) do |i|
-                        cell = @grid.get_cell(row, i)
-                    end
-                end
-            end
-        end
-
-        if !@edges.empty?
-            @edges.each do |edge|
-                edge.range.each do |offset|
-                    args = edge.horizontal_edge? ? [span_y - (edge.v1.y + (-min_y)), offset - min_x] : [span_y - (offset + (-min_y)), edge.v1.x - min_x]
-                    cell = @grid.get_cell(*args)
-                    raise "bad edge #{edge}" if cell.edge
-                    cell.edge = [edge.v1, edge.v2]
-                end
-            end
-        end
-
-        # @grid.rows.each_with_index do |row, ri|
-        #     row.each_with_index do |cell, ci|
-        #         x, y = grid_to_point(ri, ci)
-        #         cell.covered = cover?(x, y)
-        #     end
-        # end
-
-        @grid
-    end
-
     def area
-        return @area if @area
-        @area = grid.each.count(&:inside?)
-    end
-
-    def visualize(method=:colored?)
-        grid.visualize(method)
-    end
-
-    def area_alg
         area = 0
         vertical_edges.each do |edge|
             remaining = [edge.range]
@@ -451,17 +211,4 @@ File.readlines(File.join(__dir__, 'input.txt'), chomp: true).each do |line|
     polygon.append_relative(point[0], point[1], point_match[:hex])
 end
 
-# [:edge_type, :included, :covered, :inside?].each do |viz|
-#     html = <<-EOS
-# <html>
-#     <body style="background: #000; color: #fff">
-#         <pre style="position: absolute; top: 0; left: 0; opacity: 0.75">
-# #{polygon.visualize(viz)}
-#         </pre>
-#     </body>
-# </html>
-# EOS
-#     File.write(File.join(__dir__, "out-#{viz}.html"), html)
-# end
-
-puts "Area: #{polygon.area_alg}"
+puts "Area: #{polygon.area}"
